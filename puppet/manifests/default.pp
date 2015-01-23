29d0db8dbe7fc8bc186df839edccfcf531c11821
Paul Cook <paul.cook@alternateversion.com>
2015-01-23 14:19:16 -0500

    updated to ignore project dir and a working default.pp

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,5 +1,7 @@
-$rails_project = 'buckeye_vault'
-$db_names = ['buckeye_vault_development','buckeye_vault_test']
+$rails_project = 'curio'
+$db_names = ['curio_development','curio_test']
+$db_user = 'curio_user'
+$db_user_pass = 'P4ss1sR3al'
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
@@ -23,6 +25,7 @@ package { $sysPackages:
 }
 
 class { 'apache':
+  mpm_module => 'worker',
   require => Class['apt'],
 }
 
@@ -30,43 +33,14 @@ user { 'vagrant':
   ensure => present,
 }
 
- # Install RVM and some gems then passenger/apache
-class { 'rvm':
-  require => Class['apache'],
-}
-
-rvm::system_user { vagrant: }
-
-rvm_system_ruby {
-  'ruby-2.1.5':
-    ensure => present,
-    require => Class['rvm::system'],
-    default_use => true;
-}
-
-rvm_gem {
-  'ruby-2.1.4/bundler':
-    ensure => latest,
-    require => Rvm_system_ruby['ruby-2.1.5'];
-}
-
-class {
-  'rvm::passenger::apache':
-    ruby_version => 'ruby-2.1.5';
-}
-
-# class { 'apache_passenger_vhost':
-#   require => Class['install_rvm_passenger'],
-# }
-
 # # Install Postgresql and setup databases
 class install_postgres {
 
   class { 'postgresql::server': }
 
   postgresql::server::db { $db_names:
-    user => 'rails',
-    password => 'tempPass4u'
+    user => $db_user,
+    password => postgresql_password($db_user,$db_user_pass)
   }
 
   package { 'libpq-dev':
@@ -80,6 +54,31 @@ class install_postgres {
 }
 
 class { 'install_postgres': }
+
+
+# Install RVM and some gems then passenger/apache
+class { 'rvm':
+  require => Class['apache'],
+}
+
+rvm::system_user { vagrant: }
+
+rvm_system_ruby {
+  'ruby-2.1.5':
+    ensure => present,
+    require => Class['rvm::system'],
+    default_use => true;
+}
+
+class {
+  'rvm::passenger::apache':
+    version => '4.0.57',
+    ruby_version => 'ruby-2.1.5';
+}
+
+# # class { 'apache_passenger_vhost':
+# #   require => Class['install_rvm_passenger'],
+# # }
 
 # exec {
 #   'bundle rails':
